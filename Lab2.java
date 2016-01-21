214652f577e394a868193cafe2fc49333dbd170c
Ashwin Iyer <api4@pitt.edu>
2016-01-21 17:27:20 -0500

    Final Question

--- Lab2.java
+++ Lab2.java
@@ -16,15 +16,15 @@ public class Lab2 {
 	// Assume that the original man is also going to St. Ives, so
 	// the boolean variable manGoingToStIves should be true.
 	
-	boolean manGoingToStIves = !true;
+	boolean manGoingToStIves = true;
 
-	 int numWives = 8;
+	 int numWives = 7;
 
-	 int numSacksPerWife = 7;
+	 int numSacksPerWife = 7 * numWives;
 	
-	 int numCatsPerSack = 7;
+	 int numCatsPerSack = 7 * numSacksPerWife;
 
-	 int numKitsPerCat = 7;
+	 int numKitsPerCat = 7 * numCatsPerSack;
 
 	int total;
 
@@ -38,7 +38,8 @@ public class Lab2 {
 	    // all of the wives, sacks, cats, and kittens are as well, along
 	    // with the narrator.
 	    // Calculate the number going to St. Ives
-	    total = numWives + numSacksPerWife + numCatsPerSack + numKitsPerCat;
+	    	total = 2 + numWives + numSacksPerWife + numCatsPerSack + numKitsPerCat;
+	
 	} else {
 	    // Otherwise, only the narrator is going!
 	    total = 1;
@@ -48,6 +49,8 @@ public class Lab2 {
 	// "Number going to St. Ives is " and then, on the same line,
 	// the number going to St. Ives (stored in the _total_ variable).
 	// Add a statement or statements that will do this.
+	
+	System.out.println("Number going to St. Ives is: " + total);
 	
     }
     
